6a4fe7c5aa0a5a636f97254256b42f864e6939ed
Ladi Rohit <SESA673971@se.com>
2022-06-21 13:21:28 +0530

    added footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,10 +1,44 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap'
 function footer() {
   return (
-    <div>
-      Footer
+    <>
+    <div className="container-fluid text-center text-md-left pt-3" style={{"background-color": "black"}}>
+        <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+                <h5 className="text-uppercase" style={{"color": "white"}}>Footer Content</h5>
+                <p style={{"color": "gray"}}>Here you can use rows and columns to organize your footer content.</p>
+            </div>
+
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+                <h5 className="text-uppercase" style={{"color": "white"}}>Links</h5>
+                <ul className="list-unstyled">
+                    <li><a href="#!" style={{"color":"darkgray"}}>Link 1</a></li>
+                    <li><a href="#!" style={{"color":"darkgray"}}>Link 2</a></li>
+                    <li><a href="#!" style={{"color":"darkgray"}}>Link 3</a></li>
+                    <li><a href="#!" style={{"color":"darkgray"}}>Link 4</a></li>
+                </ul>
+            </div>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+                <h5 className="text-uppercase" style={{"color": "white"}}>Links</h5>
+                <ul className="list-unstyled">
+                    <li><a href="#!" style={{"color":"darkgray"}}>Link 1</a></li>
+                    <li><a href="#!" style={{"color":"darkgray"}}>Link 2</a></li>
+                    <li><a href="#!" style={{"color":"darkgray"}}>Link 3</a></li>
+                    <li><a href="#!" style={{"color":"darkgray"}}>Link 4</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
+
+    <div className="footer-copyright text-center py-3" style={{"background-color": "black","color":"white"}}>© 2022 Copyright:
+        <a href="https://se.com/" style={{"color": "white"}}> E-Hub.com</a>
+    </div>
+    </>
   )
 }
 
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,12 +1,54 @@
 import React from 'react'
 import Navbar from './navbar'
 import Footer from './footer'
+import {Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function home() {
   return (
     <div>
         <Navbar/>
-            Home
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            <h1>Home</h1>
+            <br></br>
+            {/* <Button variant="primary">Button #1</Button>
+            <Button variant="secondary" className="mx-2">Button #2</Button>
+            <Button variant="success">Button #3</Button> */}
         <Footer/>
     </div>
   )
